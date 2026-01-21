25e7b4228d653b3a3b22849af6c373f04b2bc5da
ryecries <git config --global user.email "irfanard42@gmail.com>
2026-01-21 12:24:13 +0700

    add a sync bcrypt

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res);
@@ -26,7 +26,10 @@ const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
