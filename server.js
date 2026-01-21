4caf806f592b0505fae0e4468f11c59922957604
ryecries <git config --global user.email "irfanard42@gmail.com>
2026-01-21 12:01:37 +0700

    edit fixing

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send("Hello World");
 });
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-const hash = hash(myPlaintextPassword, saltRounds, (err, hash) => {
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res);
